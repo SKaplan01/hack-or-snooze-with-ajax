0c8fec739deb24431dbe9117d383e92a2c3c2cdd
Sarah Kaplan <skaplan520@gmail.com>
2018-09-20 22:55:14 -0700

    refactored page refresh when logged in, favorites do not load properly

--- dom.js
+++ dom.js
@@ -1,9 +1,19 @@
-//if not logged in, should not be able to favorite a story
+//TODO
 //don't have favorites be a filter
-//show regular stories when you click
+//show regular stories when you click "Hack or Snooze" home
+//how do you log out? (right now--> manually clear local storage?)
 
+//currentUser is updated in 3 cases:
+// 1. user clicks login
+// 2. user clicks signup (which calls login)
+// 3. page is refreshed while a user is logged in
 let currentUser;
+
+//currentStoryList is updated whenever the page is refreshed.
+//equal to the storyList retrieved from the API by calling StoryList.getstories()
+//If a user is logged in and any of the stories displayed are in the user's favorites, these should already be marked as "favorite"
 let currentStoryList;
+
 $(document).ready(function() {
   if (localStorage.getItem('token')) {
     let decryptedToken = JSON.parse(
@@ -13,29 +23,23 @@ $(document).ready(function() {
     currentUser.loginToken = localStorage.getItem('token');
     currentUser.retrieveDetails(resp => console.log(resp));
   }
+
   displayStoryList();
+
+  //these event listeners should be on the page whether a user is logged in or not
   $('#signup').on('click', () => {
     $('#signup-form').slideToggle();
   });
   $('#submit-signup-button').on('click', signUp);
   $('#login-form').on('submit', login);
-  $('#submit-dropdown').on('click', () => {
-    $('#submit-form').slideToggle();
-  });
-  $('#submit-form').on('submit', createStory);
-  $('ol').on('click', '.fa-star', function(event) {
-    $(event.target).toggleClass('far fas');
-    $(event.target)
-      .parent()
-      .toggleClass('favorites');
-  });
-  $('ol').on('click', '.far', addFavorite);
-  $('ol').on('click', '.fas', removeFavorite);
+
   $('#favorites').on('click', () => {
     let favorites = currentUser.favorites;
+    //needs to be fixed (shouldn't be a filter that turns on and off, a separate list)
     $('#story-list li:not(.favorites)').toggleClass('li hidden-favorites');
   });
 });
+
 function getHostname(url) {
   url = url.replace('https://', '');
   url = url.replace('http://', '');
@@ -55,6 +59,11 @@ function displayStoryList() {
       smallTag.append(url);
       listItem.append(smallTag);
       $('#story-list').append(listItem);
+
+      //this is not working (I checked with story ID's that === true, but wasn't starred)
+      //currentStoryList.stories[0].storyId === currentUser.favorites[18].storyId
+      //it DOES show already favorited stories on refresh, but not right after user logs in
+      //second refresh clears the stars
       if (currentUser) {
         for (let j = 0; j < currentUser.favorites.length; j++) {
           if (
@@ -65,6 +74,11 @@ function displayStoryList() {
           }
         }
       }
+    }
+    if (currentUser) {
+      enableFavorites();
+      enableSubmitStory();
+      //NEED to REMOVE: can't log in if already logged in --> $('#login-form').on('submit', login);
     }
   });
 }
@@ -92,6 +106,9 @@ function login() {
     localStorage.setItem('token', currentUser.loginToken);
     // localStorage.setItem('currentUser', currentUser);
     currentUser.retrieveDetails(resp => console.log(resp));
+    enableFavorites();
+    enableSubmitStory();
+    $('#login-form').trigger('reset');
   });
 }
 function createStory() {
@@ -110,6 +127,32 @@ function createStory() {
   });
   $('#submit-form').trigger('reset');
 }
+
+//called if a user is logged in
+//sets event listeners on the star icons --> users can click to add/remove story from favorites
+//TODO: favorites are stored in user details in API and SHOULD repopulate when user logs in
+//it DOES show already favorited stories on refresh, but not right after user logs in
+//second refresh clears the stars
+function enableFavorites() {
+  $('ol').on('click', '.fa-star', function(event) {
+    $(event.target).toggleClass('far fas');
+    $(event.target)
+      .parent()
+      .toggleClass('favorites');
+  });
+  $('ol').on('click', '.far', addFavorite);
+  $('ol').on('click', '.fas', removeFavorite);
+}
+
+//called if a user is logged in, shows submit form
+//only users who are logged in should be able to submit a story (does nothing if not logged in)
+function enableSubmitStory() {
+  $('#submit-dropdown').on('click', () => {
+    $('#submit-form').slideToggle();
+  });
+  $('#submit-form').on('submit', createStory);
+}
+
 function addFavorite(event) {
   let storyId = currentStoryList.stories[event.target.id].storyId;
   currentUser.addFavorite(storyId, resp => {
